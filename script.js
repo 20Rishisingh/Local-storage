043b3ccb309bedebd282697dcc8f59af1133b31a
Rishi Singh Rajput <126874655+20Rishisingh@users.noreply.github.com>
2024-04-19 19:50:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const textbox = document.getElementById("txt-box");
 
 // Function to save the textarea content to local storage
 function saveLocalStrg(){
-   localStorage.setItem("savedText", textbox.value);
+   localStorage.setItem("Text", textbox.value);
 }
 
 // Check if there's existing data in local storage and populate the textarea
-if(localStorage.getItem("savedText")){
-   textbox.value = localStorage.getItem("savedText");
+if(localStorage.getItem("Text")){
+   textbox.value = localStorage.getItem("Text");
 }
 
 // Add an event listener to the textarea for input changes and save to local storage
